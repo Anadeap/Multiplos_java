3b654ec6cabf739635c314a867a17437793c66dd
KP2020-HUB <anaideandres@gmail.com>
2021-01-26 22:01:34 +0100

    Añadido comentario a la clase Multiplos del proyecto MULTIPLOS

---
+++ src/Multiplos.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+/*
+Programa java que pide dos números por teclado
+y saca por pantalla si son múltiplos o no
+*/
+
+public class Multiplos {
+    public static void main(String args[]) {
+        // clase Scanner para pedir los datos
+        Scanner entrada=new Scanner(System.in);
+        // se declaran 2 variables
+        int x,y;
+        // petición del primer número por teclado
+        System.out.println("Introduce un número");
+        x = entrada.nextInt();
+        // petición del segundo número por teclado
+        System.out.println("Introduce otro número");
+        y = entrada.nextInt();
+        // se comparan según el resto
+        // y se saca el resultado por pantalla
+        if(x%y==0){
+            System.out.println(x + " es múltiplo de " + y);
+        }
+        else{
+            System.out.println(x + " no es múltiplo de " + y);       
+        }
+    } 
+} 
+
